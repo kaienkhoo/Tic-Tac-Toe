db106dcc937842a4e013e7cd519b01107f6da602
kaienkhoo <100989619+kaienkhoo@users.noreply.github.com>
2026-01-08 22:38:05 +0800

    Add winner check and improve board state handling
    Introduced a checkWinner function to determine if the active player has won after each move. Refactored board state retrieval with getBoardState, updated placeMarker to return a boolean for move validity, and adjusted playRound logic to handle invalid moves and announce winners.

--- index.js
+++ index.js
@@ -26,19 +26,19 @@ function Gameboard() {
     const placeMarker = (row, column, player) => {
         if (board[row][column].getValue() !== ' ') {
             console.log('Cell already occupied');
-            return;
+            return false;
         }
 
         board[row][column].addMarker(player);
+        return true;
     }
 
-    const printBoard = () => {
-        const boardWithCellValues = board.map(row => row.map(cell => cell.getValue()));
-        console.log(boardWithCellValues);
+    const getBoardState = () => {
+        return board.map(row => row.map(cell => cell.getValue()));
     }
 
 
-    return { getBoard, printBoard, placeMarker };
+    return { getBoard, getBoardState, placeMarker };
 }
 
 
@@ -64,13 +64,46 @@ function GameController() {
 
     const getActivePlayer = () => activePlayer;
 
+    const checkWinner = () => {
+        const b = board.getBoardState();
+
+        for (let i = 0; i < 3; i++) {
+            if (b[i][0] === activePlayer.marker && b[i][1] === activePlayer.marker && b[i][2] === activePlayer.marker) {
+                return true;
+            }
+
+            if (b[0][i] === activePlayer.marker && b[1][i] === activePlayer.marker && b[2][i] === activePlayer.marker) {
+                return true;
+            }
+        }
+
+        if (b[0][0] === activePlayer.marker && b[1][1] === activePlayer.marker && b[2][2] === activePlayer.marker) {
+            return true;
+        }
+        if (b[0][2] === activePlayer.marker && b[1][1] === activePlayer.marker && b[2][0] === activePlayer.marker) {
+            return true;
+        }
+
+        return false;
+    }
+
     const printNewRound = () => {
-        board.printBoard();
+        console.log(board.getBoardState());
         console.log(`${getActivePlayer().name}'s turn (${getActivePlayer().marker})`);
     };
 
     const playRound = (row, column) => {
-        board.placeMarker(row, column, getActivePlayer().marker);
+        const validMove = board.placeMarker(row, column, getActivePlayer().marker);
+
+        if (!validMove) {
+            return;
+        }
+
+        if (checkWinner()) {
+            console.log(board.getBoardState());
+            console.log(`${getActivePlayer().name} wins!`);
+            return
+        }
 
         switchPlayer();
         printNewRound();
@@ -84,6 +117,11 @@ function GameController() {
 const game = GameController();
 
 game.playRound(2, 1)
+game.playRound(0, 0)
+game.playRound(1, 1)
+game.playRound(0, 1)
+game.playRound(1, 2)
+game.playRound(0, 2)
 
 
 
